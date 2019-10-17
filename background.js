5e915ab550537a7ded9f8480e39dfe8b03ffb239
yonashub <yonas.kassa@imdea.org>
2019-10-17 10:29:32 +0200

    Update background.js

--- background.js
+++ background.js
@@ -3,3 +3,15 @@ chrome.runtime.onInstalled.addListener(function() {
     console.log('The color is green.');
   });
 });
+
+function handleContextMenuClick(info, tab) {
+    //const linkUrl = info["linkUrl"];
+    alert("checking sentiment of selection: ");
+    //checkselectionsentiment(selectedtext);
+}
+
+chrome.contextMenus.create({
+    "title": "check selection sentiment",
+    "contexts": ["selection"],
+    "onclick": handleContextMenuClick
+});
